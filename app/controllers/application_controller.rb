4ccf81f961edea9633088d22aef8e641c4b8dbb7
CorrineMuriuki <Corrine.Muriuki@student.moringaschool.com>
2022-12-10 16:18:02 +0300

    Complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+  resources :sessions, only: [:create]
   include ActionController::Cookies
 
 end
---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+    #macro
+    has_secure_password
+end
